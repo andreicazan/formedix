31147baa17e6292dbbadf289560aecbeaf326d67
Andrei Cazan <andrei.cazan@ebs.msft>
2022-05-10 18:51:21 +0300

    Add search photos component

---
+++ src/app/search-photos/search-photos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-photos',
+  templateUrl: './search-photos.component.html',
+  styleUrls: ['./search-photos.component.css']
+})
+export class SearchPhotosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
